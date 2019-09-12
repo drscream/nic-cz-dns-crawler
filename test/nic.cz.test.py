43133b5323832000c829a5a2866cfa8eb822c1e6
Jiří Helebrant <jiri.helebrant@nic.cz>
2019-09-12 08:08:37 +0200

    CI: keep test output in gitlab's artifacts

--- test/nic.cz.test.py
+++ test/nic.cz.test.py
@@ -4,8 +4,6 @@ import json
 r = process_domain("nic.cz")
 
 print(json.dumps(r))
-print()
-print(r["DNS_LOCAL"]["DNS_AUTH"])
 
 assert sorted(r["DNS_LOCAL"]["DNS_AUTH"]) == ["a.ns.nic.cz.", "b.ns.nic.cz.", "d.ns.nic.cz."]
 assert r["WEB"]["WEB4_80_VENDOR"] == {"value": "nginx"}
