5bf7e4a54fae60802ec980651fd29950f56ece74
Jiří Helebrant <jiri.helebrant@nic.cz>
2019-09-20 15:19:38 +0200

    Add more values to the automated test

--- test/nic.cz.test.py
+++ test/nic.cz.test.py
@@ -10,7 +10,11 @@ assert r["results"]["DNS_LOCAL"]["DNS_AUTH"] == [
     {"value": "b.ns.nic.cz."},
     {"value": "d.ns.nic.cz."}
 ]
+
 assert r["results"]["WEB"]["WEB4_80_VENDOR"][0]["value"] == "nginx"
+assert r["results"]["DNS_LOCAL"]["WEB4"][0]["ripe"]["netname"] == "CZ-NIC-I"
+assert r["results"]["DNS_LOCAL"]["DNSSEC"]["valid"]
+assert r["results"]["DNS_LOCAL"]["DS"][0]["algorithm"] == "ECDSAP256SHA256"
 assert r["results"]["DNS_LOCAL"]["WEB4"][0]["geoip"] == {
     "country": "CZ",
     "asn": 25192,
