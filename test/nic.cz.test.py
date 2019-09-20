a7a199f061b0f56317cff052937555750d39eb79
Jiří Helebrant <jiri.helebrant@nic.cz>
2019-09-20 15:27:58 +0200

    Test: sort the DNS_AUTH list before asserting

--- test/nic.cz.test.py
+++ test/nic.cz.test.py
@@ -5,18 +5,12 @@ r = process_domain("nic.cz")
 
 print(json.dumps(r))
 
-assert r["results"]["DNS_LOCAL"]["DNS_AUTH"] == [
-    {"value": "a.ns.nic.cz."},
-    {"value": "b.ns.nic.cz."},
-    {"value": "d.ns.nic.cz."}
-]
+assert r["results"]["DNS_LOCAL"]["DNS_AUTH"] == sorted(
+    [{"value": "a.ns.nic.cz."}, {"value": "b.ns.nic.cz."}, {"value": "d.ns.nic.cz."}], key=lambda k: k["value"]
+)
 
 assert r["results"]["WEB"]["WEB4_80_VENDOR"][0]["value"] == "nginx"
 assert r["results"]["DNS_LOCAL"]["WEB4"][0]["ripe"]["netname"] == "CZ-NIC-I"
 assert r["results"]["DNS_LOCAL"]["DNSSEC"]["valid"]
 assert r["results"]["DNS_LOCAL"]["DS"][0]["algorithm"] == "ECDSAP256SHA256"
-assert r["results"]["DNS_LOCAL"]["WEB4"][0]["geoip"] == {
-    "country": "CZ",
-    "asn": 25192,
-    "org": "CZ.NIC, z.s.p.o.",
-}
+assert r["results"]["DNS_LOCAL"]["WEB4"][0]["geoip"] == {"country": "CZ", "asn": 25192, "org": "CZ.NIC, z.s.p.o."}
