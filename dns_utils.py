e796352e6b4a6c05854cb7a3ec3e6ad4810f58c7
Jiří Helebrant <jiri.helebrant@nic.cz>
2019-10-03 17:06:58 +0200

    DNSSEC: check for timeouts before checking for truncated replies

--- dns_utils.py
+++ dns_utils.py
@@ -18,10 +18,10 @@ def check_dnssec(domain, resolver):
     q = dns.message.make_query(sub, "DNSKEY", want_dnssec=True)
     try:
         response = dns.query.udp(q, resolver.nameservers[0], resolver.timeout)
-    except dns.message.Truncated:
-        response = dns.query.tcp(q, resolver.nameservers[0], resolver.timeout)
     except dns.exception.Timeout:
         return {"valid": None, "error": "timeout"}
+    except dns.message.Truncated:
+        response = dns.query.tcp(q, resolver.nameservers[0], resolver.timeout)
 
     rcode = response.rcode()
 
