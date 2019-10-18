77600ef950c11f4f3a40f627b503e160214b6ac9
Jiří Helebrant <jiri.helebrant@nic.cz>
2019-10-18 14:40:20 +0200

    IP validation utils

--- ip_utils.py
+++ ip_utils.py
@@ -7,3 +7,15 @@ def is_valid_ipv6_address(ip):
     except ValueError:
         return False
     return True
+
+
+def is_valid_ipv4_address(ip):
+    try:
+        ipaddress.IPv4Address(ip)
+    except ValueError:
+        return False
+    return True
+
+
+def is_valid_ip_address(ip):
+    return is_valid_ipv4_address(ip) or is_valid_ipv6_address(ip)
