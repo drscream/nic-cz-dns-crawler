5a7e4cedfd08f3d58e3214e4d2941dba96412f71
Jiří Helebrant <jiri.helebrant@nic.cz>
2019-11-29 09:18:16 +0100

    Update tests

--- test/nic.cz.test.py
+++ test/nic.cz.test.py
@@ -1,4 +1,4 @@
-from crawl import process_domain
+from dns_crawler.crawl import process_domain
 import json
 
 r = process_domain("nic.cz")
@@ -14,8 +14,9 @@ assert sort_by_value(r["results"]["DNS_LOCAL"]["NS_AUTH"]) == sort_by_value(
     [{"value": "a.ns.nic.cz."}, {"value": "b.ns.nic.cz."}, {"value": "d.ns.nic.cz."}]
 )
 assert r["results"]["WEB"]["WEB4_80"][0]["status"] == 301
-assert r["results"]["WEB"]["WEB4_80"][0]["headers"]["server"][0] == "nginx"
-assert r["results"]["WEB"]["WEB4_443"][0]["http2"]
+assert r["results"]["WEB"]["WEB4_80"][0]["headers"]["server"] == "nginx"
+assert r["results"]["WEB"]["WEB4_443"][0]["tls"]["alpn_protocol"] == "h2"
+assert r["results"]["WEB"]["WEB4_443"][0]["tls"]["cert"]["subject"] == "nic.cz"
 assert r["results"]["WEB"]["WEB4_443_www"][0]["status"] == 200
 assert r["results"]["DNS_LOCAL"]["DNSSEC"]["valid"]
 assert r["results"]["DNS_LOCAL"]["DS"][0]["algorithm"] == "ECDSAP256SHA256"
