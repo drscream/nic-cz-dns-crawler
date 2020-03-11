653e795fc29ee52705c951f8a3d3c217560b5912
Jiří Helebrant <jiri.helebrant@nic.cz>
2020-03-11 11:23:00 +0100

    Update test asserts to match the new result format

--- test/nic.cz.test.py
+++ test/nic.cz.test.py
@@ -31,13 +31,14 @@ def sort_by_value(list):
 assert sort_by_value(r["results"]["DNS_LOCAL"]["NS_AUTH"]) == sort_by_value(
     [{"value": "a.ns.nic.cz."}, {"value": "b.ns.nic.cz."}, {"value": "d.ns.nic.cz."}]
 )
-assert r["results"]["WEB"]["WEB4_80"][0]["status"] == 301
-assert r["results"]["WEB"]["WEB4_80"][0]["headers"]["server"] == "nginx"
-assert r["results"]["WEB"]["WEB4_443"][0]["tls"]["alpn_protocol"] == "h2"
-assert r["results"]["WEB"]["WEB4_443"][0]["tls"]["cert"]["subject"]["CN"] == "nic.cz"
-assert r["results"]["WEB"]["WEB4_443"][0]["tls"]["cert"]["version"] == 3
-assert r["results"]["WEB"]["WEB4_443"][0]["tls"]["cert"]["algorithm"] == "sha256"
-assert r["results"]["WEB"]["WEB4_443_www"][0]["status"] == 200
+assert r["results"]["WEB"]["WEB4_80"][0]["steps"][0]["status"] == 301
+assert r["results"]["WEB"]["WEB4_80"][0]["steps"][0]["headers"]["server"] == "nginx"
+
+assert r["results"]["WEB"]["WEB4_443"][0]["steps"][-1]["cert"][0]["subject"]["CN"] == "nic.cz"
+assert r["results"]["WEB"]["WEB4_443"][0]["steps"][-1]["cert"][0]["version"] == 3
+assert r["results"]["WEB"]["WEB4_443"][0]["steps"][-1]["cert"][0]["algorithm"] == "sha256"
+assert r["results"]["WEB"]["WEB4_443_www"][0]["steps"][-1]["status"] == 200
+
 assert r["results"]["DNS_LOCAL"]["DNSSEC"]["valid"]
 assert r["results"]["DNS_LOCAL"]["DS"][0]["algorithm"] == "ECDSAP256SHA256"
 assert r["results"]["DNS_LOCAL"]["WEB4"][0]["geoip"] == {"country": "CZ", "asn": 25192, "org": "CZ.NIC, z.s.p.o."}
