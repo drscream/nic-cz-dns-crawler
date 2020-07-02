1502ccbbb24577878eceb4bd81ff676458cdccc3
Jiří Helebrant <jiri.helebrant@nic.cz>
2020-07-02 16:54:27 +0200

    MAIL: cache the server responses by IPs, too (not just hostnames), so we make less connections overall

--- dns_crawler/mail_utils.py
+++ dns_crawler/mail_utils.py
@@ -21,38 +21,67 @@ import json
 import socket
 
 from .dns_utils import get_record, parse_tlsa
+from .ip_utils import is_valid_ipv4_address, is_valid_ipv6_address
+
+
+def get_smtp_banner(host_ip, port, timeout):
+    result = {"port": port}
+    if is_valid_ipv4_address(host_ip):
+        inet = socket.AF_INET
+    elif is_valid_ipv6_address(host_ip):
+        inet = socket.AF_INET6
+    else:
+        return None
+    try:
+        s = socket.socket(inet, socket.SOCK_STREAM)
+        s.settimeout(timeout)
+        s.connect((host_ip, port))
+    except (OSError, socket.timeout, ConnectionRefusedError) as e:
+        result["error"] = str(e)
+    else:
+        try:
+            banner = s.recv(1024).decode().replace("\r\n", "")
+            result["banner"] = banner
+        except Exception as e:
+            result["error"] = str(e)
+        s.close()
+        return result
 
 
 def get_mailserver_info(host, ports, timeout, get_banners, cache_timeout, resolver, redis):
-    cache_key = f"cache-mail-{host}"
+    cache_key_host = f"cache-mail-host-{host}"
     if redis is not None:
-        cached = redis.get(cache_key)
-        if cached is not None:
-            redis.expire(cache_key, cache_timeout)
-            return json.loads(cached.decode("utf-8"))
+        cached_host = redis.get(cache_key_host)
+        if cached_host is not None:
+            redis.expire(cache_key_host, cache_timeout)
+            return json.loads(cached_host.decode("utf-8"))
     result = {}
     result["host"] = host
     result["TLSA"] = {}
-    if get_banners:
-        result["banners"] = {}
-        result["errors"] = {}
     for port in ports:
         result["TLSA"][port] = parse_tlsa(get_record(f"_{port}._tcp." + host, "TLSA", resolver))
-        if get_banners:
-            try:
-                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                s.settimeout(timeout)
-                s.connect((host, 25))
-            except (OSError, socket.timeout, ConnectionRefusedError) as e:
-                result["errors"][port] = str(e)
-            else:
-                try:
-                    result["banners"][port] = s.recv(1024).decode().replace("\r\n", "")
-                except Exception as e:
-                    result["errors"][port] = str(e)
-                s.close()
+    if get_banners:
+        result["banners"] = []
+        host_ip4s = get_record(host, "A", resolver)
+        host_ip6s = get_record(host, "AAAA", resolver)
+        host_ips = host_ip4s or [] + host_ip6s or []
+        for host_ip in host_ips:
+            cache_key_ip = f"cache-mail-ip-{host_ip}"
+            if redis is not None:
+                cached_ip = redis.get(cache_key_ip)
+                if cached_ip is not None:
+                    redis.expire(cache_key_ip, cache_timeout)
+                    result["banners"].append(json.loads(cached_ip.decode("utf-8")))
+                    continue
+            host_ip = host_ip["value"]
+            ip_banners = {"ip": host_ip, "banners": []}
+            for port in ports:
+                ip_banners["banners"].append(get_smtp_banner(host_ip, port, timeout))
+            if redis is not None:
+                redis.set(cache_key_ip, json.dumps(ip_banners), ex=cache_timeout)
+            result["banners"].append(ip_banners)
     if redis is not None:
-        redis.set(cache_key, json.dumps(result), ex=cache_timeout)
+        redis.set(cache_key_host, json.dumps(result), ex=cache_timeout)
     return result
 
 
