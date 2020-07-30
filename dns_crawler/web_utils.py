62d2ae52aaaf1ea5f5234d2eff62f88581294726
Jiří Helebrant <jiri.helebrant@nic.cz>
2020-07-30 16:46:14 +0200

    Split HTML stripper into a separate package (https://pypi.org/project/html-stripper/)

--- dns_crawler/web_utils.py
+++ dns_crawler/web_utils.py
@@ -22,12 +22,14 @@ from html.parser import HTMLParser
 from itertools import takewhile
 from urllib.parse import unquote, urljoin, urlparse
 
-import cert_human
-import icu
 import idna
 import requests
 import urllib3
+
+import cert_human
+import icu
 from forcediphttpsadapter.adapters import ForcedIPHTTPSAdapter
+from html_stripper import strip_multiple_newlines, strip_tags
 from requests_toolbelt.adapters.source import SourceAddressAdapter
 
 from .certificate import parse_cert
@@ -118,69 +120,6 @@ def headers_look_like_binary(headers):
     if "accept-range" in headers and headers["accept-range"] == "bytes":
         return True
     return False
-
-
-_markedsectionclose = re.compile(r"]\s*]\s*>")
-_msmarkedsectionclose = re.compile(r"]\s*>")
-_markedsectionclose2 = re.compile(r">")
-
-
-class HTMLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.strict = False
-        self.convert_charrefs = True
-        self.fed = []
-
-    def handle_starttag(self, tag, attrs):
-        if tag in ["script", "style"]:
-            return ""
-
-    def handle_data(self, d):
-        tag = self.get_starttag_text()
-        if not tag:
-            self.fed.append(d)
-        else:
-            if tag.startswith("<script") or tag.startswith("<style"):
-                pass
-            else:
-                self.fed.append(d)
-
-    def parse_marked_section(self, i, report=1):
-        match = None
-        rawdata = self.rawdata
-        assert rawdata[i:i+3] == "<![", "unexpected call to parse_marked_section()"
-        sectName, j = self._scan_name(i+3, i)
-        if j < 0:
-            return j
-        if sectName in ["temp", "cdata", "ignore", "include", "rcdata"]:
-            # look for standard ]]> ending
-            match = _markedsectionclose.search(rawdata, i+3)
-        elif sectName in ["if", "else", "endif"]:
-            # look for MS Office ]> ending
-            match = _msmarkedsectionclose.search(rawdata, i+3)
-        else:
-            # probably some broken tag, just skip to closing ">"
-            match = _markedsectionclose2.search(rawdata, i+1)
-        if not match:
-            return -1
-        if report:
-            j = match.start(0)
-            self.unknown_decl(rawdata[i+3: j])
-        return match.end(0)
-
-    def get_data(self):
-        return " ".join(self.fed)
-
-
-def strip_tags(html):
-    s = HTMLStripper()
-    s.feed(html)
-    return s.get_data()
-
-
-def strip_newlines(text):
-    return re.sub(r"(\r?\n *)+", "\n", re.sub(r" {2,}", "", re.sub(r"\t+", "", text))).strip()
 
 
 def emsg(e):
@@ -368,7 +307,7 @@ def get_webserver_info(domain, ips, config, source_ip, ipv6=False, tls=False):
                     content = None
                 if content and not content_is_binary:
                     if config["web"]["strip_html"]:
-                        content = strip_newlines(strip_tags(content))
+                        content = strip_multiple_newlines(strip_tags(content))
                     if len(content) > content_size_limit:
                         content = content[:content_size_limit]
                 step["content"] = content
